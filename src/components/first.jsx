0506672a04d879a987b2925c2b9f8282c73663c7
Mustafa Badr <mustafabadr879@gmail.com>
2023-09-17 21:07:25 +0300

    initial Commit, Adding bootstrap packages.

---
+++ src/components/first.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const FirstComp = () => {
+  return (
+    <>
+      <h2>hello</h2>
+      <button className="btn btn-danger">Danger</button>
+    </>
+  );
+};
+
+export default FirstComp;
