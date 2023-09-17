1cc03d93634c117d50d2e0b08664e6a68d6ff29a
Mustafa Badr <mustafabadr879@gmail.com>
2023-09-17 21:19:20 +0300

    Add react-router-dom5 & Create homePage component.

---
+++ src/pages/homePage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import MyNavbar from "../components/MyNavBar";
+const HomePage = () => {
+  return (
+    <>
+     <MyNavbar/>
+    </>
+  );
+};
+
+export default HomePage;
